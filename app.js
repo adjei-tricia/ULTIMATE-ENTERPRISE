5d00e42464e646ef52f0e005453cb355c7a08305
adjei-tricia <patriciaadjei831@gmail.com>
2026-05-28 10:35:51 +0000

    new changes

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const loginStep = document.getElementById('login-step');
 const orderStep = document.getElementById('order-step');
+const WHATSAPP_NUMBER = '0598663389'; // Replace with your WhatsApp number
+let emailInput = document.getElementById('email');
+let passwordInput = document.getElementById('password');
+let nameInput = document.getElementById('name');
+let phoneInput = document.getElementById('phone');
+let addressInput = document.getElementById('address');
 
 function handleNextStep(event) {
     event.preventDefault();
@@ -14,5 +20,29 @@ function handleBackStep() {
 
 function handleFinalOrder(event) {
     event.preventDefault();
-    alert("Order Received! We will call you shortly to arrange delivery.");
+
+    const formData = {
+        name: nameInput.value,
+        phone: phoneInput.value,
+        address: addressInput.value
+    };
+
+    const message =
+        `Name: ${formData.name}\n` +
+        `Phone: ${formData.phone}\n` +
+        `Address: ${formData.address}`;
+
+    const url = `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`;
+
+    window.open(url, '_blank');
 }
+// orderStep.addEventListener("submit",(e)=>{
+//     e.preventDefault();
+//     const formData = {
+//         name: nameInput.value,
+//         phone: phoneInput.value,
+//         address: addressInput.value
+//     }
+//     // when submit open whatsApp with the provided data
+//     document.location.href = `https://wa.me/${WHATSAPP_NUMBER}?text=Name:%20${formData.name}%0APhone:%20${formData.phone}%0AAddress:%20${formData.address}`;    
+// })
